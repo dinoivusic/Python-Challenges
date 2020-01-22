77f4af78144cdd37a4560893eb4dbf2ba5d0d2cd
dino86 <dinoivusic@gmail.com>
2020-01-22 19:46:26 +0000

    Day 15 - Two Sum - Given an array of integers, return indices of the two numbers such that they add up to a specific target.

---
+++ day15.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            if target - x in d:
+                return (d[target - x], i )
+            d[x] = i
+        return None
