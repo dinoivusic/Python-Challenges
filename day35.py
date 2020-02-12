31821fff04a534037c02f68024b6e0a69ecdb17a
dino86 <dinoivusic@gmail.com>
2020-02-12 18:22:41 +0000

    Day 35 - Create a function that takes in a list of intervals and returns how many intervals overlap with a given point. An interval overlaps a particular point if the point exists inside the interval, or on the interval's boundary.

---
+++ day35.py
@@ -0,0 +1,8 @@
+def overlap(arr,num):
+    count = 0
+    for i in range(len(arr)):
+        if arr[i][0] == num or arr[i][1] == num:
+            count+=1
+        if num > arr[i][0] and num < arr[i][1]:
+            count+=1
+    return count
